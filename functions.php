277ad517104973c181196a63a68bc9a09f420015
FranciscoUrsua <francisco.ursua@proton.me>
2025-10-10 10:30:14 +0000

    Include navwalker

--- functions.php
+++ functions.php
@@ -50,5 +50,69 @@ function bachpedia_theme_setup() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('custom-logo');
+
+    register_nav_menus([
+        'primary' => __('Primary Menu', 'bachpedia'),
+        'footer' => __('Footer Menu', 'bachpedia'),
+    ]);
+
 }
 add_action('after_setup_theme', 'bachpedia_theme_setup');
+
+// Walker para menús Bootstrap 5
+class Bootstrap_Walker_Nav_Menu extends Walker_Nav_Menu {
+    /**
+     * Inicia el elemento li.
+     */
+    public function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
+        $indent = ($depth) ? str_repeat("\t", $depth) : '';
+        $classes = empty($item->classes) ? [] : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        // Añade clases Bootstrap: dropdown para parents
+        if (in_array('menu-item-has-children', $classes)) {
+            $classes[] = 'dropdown';
+            $args->link_before = '<span class="nav-link dropdown-toggle" href="' . esc_url($item->url) . '" role="button" data-bs-toggle="dropdown" aria-expanded="false">' . $args->link_before;
+            $args->link_after = '</span>' . $args->link_after;
+        } else {
+            $classes[] = 'nav-item';
+        }
+
+        $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+        $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+
+        $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
+        $id = $id ? ' id="' . esc_attr($id) . '"' : '';
+
+        $output .= $indent . '<li' . $id . $class_names . '>';
+
+        $attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+        $attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+        $attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+        $attributes .= !empty($item->url) ? ' href="' . esc_url($item->url) . '"' : '';
+
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+    }
+
+    /**
+     * Inicia el submenú ul.
+     */
+    public function start_lvl(&$output, $depth = 0, $args = null) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"dropdown-menu\">\n";
+    }
+
+    /**
+     * Termina el submenú ul.
+     */
+    public function end_lvl(&$output, $depth = 0, $args = null) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n";
+    }
+}
